c9b50efb8e8466db4e36bfb13d9c5a00dd7a6283
cshen6 <cshen6@jhu.edu>
2017-01-03 18:20:37 -0500

    paper revision done

--- Code/Experiments/run_All.m
+++ Code/Experiments/run_All.m
@@ -4,7 +4,7 @@ if nargin<1
 matchingMethod=1;
 end
 tran=1000;
-reps=50;
+reps=100;
 run_Swiss_Roll(1,matchingMethod,tran,reps);
 run_Swiss_Roll(2,matchingMethod,tran,reps);
 run_Swiss_Roll(3,matchingMethod,tran,reps);
